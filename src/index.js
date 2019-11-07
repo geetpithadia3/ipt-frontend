1d5a723bc1f252fdb88b1c71adaff87e2cff7616
kananipratik <56324101+kananipratik@users.noreply.github.com>
2019-11-07 17:26:15 -0500

    dragdrop

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Login from './components/login';
 import SignUp from './components/register';
+import DragDrop from './components/DragDrop';
 
 ReactDOM.render(
-    <SignUp/>,
+    <DragDrop/>,
     document.getElementById('root')
 );
