2fabd5da17b8853165df179bfd0dc4639b57941c
geetpithadia3 <gpithadia@gmail.com>
2019-11-09 13:20:02 -0500

    Charts init

---
+++ src/components/charts/barchart.js
@@ -0,0 +1,11 @@
+import victory from "victory"
+import VictoryBar from "victory";
+
+<VictoryChart>
+        <VictoryBar data={[
+      { x: "lizard", y: 1234 },
+      { x: "snake", y: 2048 },
+      { x: "crocodile", y: 2600 },
+      { x: "alligator", y: 9000 },
+    ]} />
+      </VictoryChart>
